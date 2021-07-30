15a9728e16c41c531783d2c4ebffef7ff0343c7c
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-07-30 19:02:11 +0300

    Simple ESlint check

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: 'src/index.html',
     }),
     new MiniCssExtractPlugin(),
   ],
